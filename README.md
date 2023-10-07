1c26ce8bf63f9f85dc1837be0ab513678f4b64ac
Aaron <avtholl@hotmail.com>
2023-10-07 17:44:16 -0600

    Add build script and readme.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tollski's Hardcore Helper
+This is a World of Warcaraft Classic addon to help players stay alive.
+
+## Features
+
+Options to notify the player when:  
+  * The player or a group member disconnects*.
+  * The player or a nearby player is affected by certain threat-altering effects.
+  * The player or a group member enters combat.
+  * The player or a group member has low health.
+  * A group members casts certain spells (e.g. Hearthstone).
+  * A group member is logging out*.
+  * A group member goes offline.
+  
+\* Requires group member to have Tollski's HardCore Helper installed
+
+Options to automatically send chat messages to the player's group when:
+  * The player's has critically low health.
+  * The player casts certain spells (e.g. Hearthstone).
+  * The player is logging out.
---
+++ build.ps1
@@ -0,0 +1,41 @@
+$addonVersionLineVanilla = Select-String -Pattern "## Version" -Path ".\TollskisHardcoreHelper_Vanilla.toc"
+$addonVersionVanilla = $addonVersionLineVanilla.ToString().Substring($addonVersionLineVanilla.ToString().LastIndexOf(" ") + 1)
+$addonVersionLineWotlk = Select-String -Pattern "## Version" -Path ".\TollskisHardcoreHelper_Wrath.toc"
+$addonVersionWotlk = $addonVersionLineWotlk.ToString().Substring($addonVersionLineWotlk.ToString().LastIndexOf(" ") + 1)
+
+if ($addonVersionVanilla -ne $addonVersionWotlk) {
+  throw "Versions don't match in TOC files."
+}
+
+$outputDirectoryPath = ".\Deploys"
+$outputFileName = "TollskisHardcoreHelper_$addonVersionVanilla"
+if ((git branch).IndexOf("* master") -lt 0 -or (git status --porcelain).length -ne 0) {
+  Write-Host "You are on a development branch or have uncommited changes."
+  $currentDateTime = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+  $outputFileName = "$outputFileName-dev-$currentDateTime"
+}
+
+$outputFilePath = "$outputDirectoryPath\$outputFileName.zip"
+
+if (Test-Path -Path "$outputFilePath") {
+  throw "Output file with the same name already exists."
+}
+
+$tempDirectoryPath = "$outputDirectoryPath\Temp"
+$tempSubDirectoryPath = "$tempDirectoryPath\TollskisHardcoreHelper"
+New-Item -Path ".\Deploys\Temp" -Name "TollskisHardcoreHelper" -ItemType "directory" | Out-Null
+
+Copy-Item ".\*" -Destination "$tempSubDirectoryPath" -Include *.lua,*.md,*.toc
+Copy-Item ".\resources" -Destination "$tempSubDirectoryPath" -Recurse
+
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+  throw "7 zip file '$7zipPath' not found"
+}
+
+Set-Alias 7zip $7zipPath
+7zip a $outputFilePath $tempSubDirectoryPath
+
+Remove-Item $tempDirectoryPath -Recurse
+
+Write-Host "Successfully created deployment package: '$outputFilePath'"
